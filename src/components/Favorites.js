d9d4a51f2eed14aa9691f3b406f6b6bad3f04e2d
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-16 17:38:50 -0700

    create favoritesApi

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,36 +2,28 @@ import React, { useContext, useEffect, useCallback } from "react";
 import { Grid, Typography } from "@material-ui/core";
 import "../styles.css";
 import { AppHeader } from "./AppHeader";
-import { constructHeader } from "../utils";
 import { RescueCard } from "./RescueCard";
 import { RescuesContext } from "../contexts/rescuesContext";
 import Loading from "./Loading";
-import { favoritesUrl } from "../server/api/apiConfig";
+import { fetchFavorites, removeFavorite } from "../server/api/favoritesApi";
 
 export const Favorites = () => {
   const { state, dispatch } = useContext(RescuesContext);
 
-  const fetchFavorites = useCallback(() => {
-    fetch(favoritesUrl, { headers: constructHeader() })
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({ type: 'setRescues', value: data.favorites });
-        const favoriteIds = data.favorites.map(favorite => favorite.id);
-        dispatch({ type: 'setFavorites', value: favoriteIds });
-        dispatch({ type: 'setLoading', value: false });
-      });
+  const fetchAndSetFavorites = useCallback(async () => {
+    const data = await fetchFavorites();
+    dispatch({ type: 'setRescues', value: data.favorites });
+    const favoriteIds = data.favorites.map(favorite => favorite.id);
+    dispatch({ type: 'setFavorites', value: favoriteIds });
+    dispatch({ type: 'setLoading', value: false });
   }, [dispatch]);
 
   useEffect(() => {
-    fetchFavorites();
-  }, [fetchFavorites]);
+    fetchAndSetFavorites();
+  }, [fetchAndSetFavorites]);
 
   const handleRemoveFavorite = useCallback(async (id) => {
-    const favResponse = await fetch(`${favoritesUrl}/${id}`, {
-      method: 'DELETE',
-      headers: constructHeader(),
-    });
-    if (favResponse.ok) {
+    if (await removeFavorite(id)) {
       dispatch({ type: 'removeFromFavorites', id });
     }
     else {
---
+++ src/server/api/favoritesApi.js
@@ -0,0 +1,15 @@
+import { constructHeader } from "../../utils";
+import { favoritesUrl } from "./apiConfig";
+
+export const fetchFavorites = async () => {
+  const response = await fetch(favoritesUrl, { headers: constructHeader() });
+  return response.json();
+};
+
+export const removeFavorite = async (id) => {
+  const response = await fetch(`${favoritesUrl}/${id}`, {
+    method: 'DELETE',
+    headers: constructHeader(),
+  });
+  return response.ok;
+};
