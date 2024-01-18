25adf66ad2191dcc61d2721260bca5081192d781
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-18 13:26:59 -0700

    Preload images to improve performance

--- src/components/rescues/RescueImage.js
+++ src/components/rescues/RescueImage.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getImageUrl } from "../../utils/utils";
 
-const RescueImage = ({ type, image, name, width, height }) => (
-  <img
-    src={getImageUrl(type, image)}
-    alt={image ? `${name}'s picture` : `${name}'s placeholder picture`}
-    style={{ borderRadius: '50%', width, height, objectFit: 'cover' }}
-  />
-);
+const RescueImage = ({ type, image, name, width, height }) => {
+  useEffect(() => {
+    const img = new Image();
+    img.src = getImageUrl(type, image);
+  }, [type, image]);
+
+  return (
+    <img
+      src={getImageUrl(type, image)}
+      alt={image ? `${name}'s picture` : `${name}'s placeholder picture`}
+      style={{ borderRadius: '50%', width, height, objectFit: 'cover' }}
+    />
+  );
+};
 
 export default RescueImage;
