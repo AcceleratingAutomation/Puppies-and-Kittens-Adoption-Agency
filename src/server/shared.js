2322283f5978598aae093ccfea5f2c77b4b6fed3
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-18 13:44:27 -0700

    Organize functions in shared.js

--- src/server/shared.js
+++ src/server/shared.js
@@ -7,111 +7,7 @@ const Constants = require("./constants");
 const fs = require('fs');
 const path = require('path');
 
-var getUserByUsername = (exports.getUserByUsername = async function (username) {
-  try {
-    const allUsers = await jsonfile.readFile(usersDB);
-    const filteredUserArray = allUsers.filter(
-      (user) => user.username === username
-    );
-    return filteredUserArray.length === 0 ? {} : filteredUserArray[0];
-  } catch (err) {
-    console.log("Error reading users: ", err.message);
-  }
-});
-
-exports.isEmptyObject = (object) => Object.entries(object).length === 0;
-
-exports.isPasswordCorrect = async function (key, password) {
-  return bcrypt.compare(password, key).then((result) => result);
-};
-
-exports.getRescueDetails = async function (id) {
-  try {
-    const allRescues = await jsonfile.readFile(rescuesDB);
-    const filteredRescueArray = allRescues.filter(
-      (rescue) => rescue.id === id
-    );
-    return filteredRescueArray.length === 0 ? {} : filteredRescueArray[0];
-  } catch (err) {
-    console.log("Error reading rescues: ", err.message);
-  }
-};
-
-exports.getAllRescues = async function () {
-  try {
-    return await jsonfile.readFile(rescuesDB);
-  } catch (err) {
-    console.log("Error reading rescues: ", err);
-  }
-};
-
-exports.getAllUsers = async function () {
-  try {
-    const allUsers = await jsonfile.readFile(usersDB);
-    let updatedUsers = [];
-    allUsers.forEach((user) => {
-      updatedUsers.push({
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        role: user.role,
-      });
-    });
-    return updatedUsers;
-  } catch (err) {
-    console.log("Error reading users from datastore ", err.message);
-  }
-};
-
-
-
-const getUsernameFromToken = (token) => {
-  if (!token) {
-    throw new Error('Token is not provided');
-  }
-
-  const decodedToken = jwt.decode(token);
-  if (!decodedToken) {
-    throw new Error('Token is not valid');
-  }
-
-  return decodedToken["sub"];
-};
-
-exports.getAudienceFromToken = (token) => jwt.decode(token)["aud"];
-
-exports.generateToken = async function (prevToken, userName) {
-  const name = userName || getUsernameFromToken(prevToken);
-  const user = await getUserByUsername(name);
-  const options = {
-    algorithm: process.env.ALGORITHM,
-    expiresIn: process.env.EXPIRY,
-    issuer: process.env.ISSUER,
-    subject: userName || user.username,
-    audience:
-      user.role === "admin"
-        ? Constants.JWT_OPTIONS.ADMIN_AUDIENCE
-        : Constants.JWT_OPTIONS.MEMBER_AUDIENCE,
-  };
-  return jwt.sign({}, process.env.SECRET, options);
-};
-
-exports.verifyToken = (req, res, next) => {
-  if (!req.headers.authorization)
-    res.status(401).send({ message: "Not authorized to access data" });
-  else {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token)
-      res.status(401).send({ message: "Not Authorized to access data" });
-    else {
-      jwt.verify(token, process.env.SECRET, function (err) {
-        if (err) {
-          res.status(401).send({ message: "Please login again" });
-        } else next();
-      });
-    }
-  }
-};
+// Favorites
 
 exports.getFavoriteRescuesForUser = async function (token) {
   const username = getUsernameFromToken(token);
@@ -176,6 +72,52 @@ exports.addFavoriteRescue = (token, rescueId) => {
   });
 };
 
+exports.deleteFavorite = (token, rescueId) => {
+  return new Promise((resolve, reject) => {
+    const username = getUsernameFromToken(token);
+
+    const usersPath = path.join(__dirname, usersDB);
+    const users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
+
+    const user = users.find(user => user.username === username);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    user.favorite = user.favorite.filter(favorite => favorite !== rescueId);
+
+    fs.writeFile(usersPath, JSON.stringify(users, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(user.favorite);
+      }
+    });
+  });
+}
+
+// Rescues
+
+exports.getRescueDetails = async function (id) {
+  try {
+    const allRescues = await jsonfile.readFile(rescuesDB);
+    const filteredRescueArray = allRescues.filter(
+      (rescue) => rescue.id === id
+    );
+    return filteredRescueArray.length === 0 ? {} : filteredRescueArray[0];
+  } catch (err) {
+    console.log("Error reading rescues: ", err.message);
+  }
+};
+
+exports.getAllRescues = async function () {
+  try {
+    return await jsonfile.readFile(rescuesDB);
+  } catch (err) {
+    console.log("Error reading rescues: ", err);
+  }
+};
+
 exports.deleteRescue = (id) => {
   return new Promise((resolve, reject) => {
     // Read the existing rescues
@@ -210,29 +152,101 @@ exports.deleteRescue = (id) => {
   });
 };
 
-exports.deleteFavorite = (token, rescueId) => {
-  return new Promise((resolve, reject) => {
-    const username = getUsernameFromToken(token);
+// Canidates
 
-    const usersPath = path.join(__dirname, usersDB);
-    const users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
+// Fosters
 
-    const user = users.find(user => user.username === username);
-    if (!user) {
-      throw new Error('User not found');
-    }
+// Veterinarians
 
-    user.favorite = user.favorite.filter(favorite => favorite !== rescueId);
+// Users
 
-    fs.writeFile(usersPath, JSON.stringify(users, null, 2), (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(user.favorite);
-      }
+var getUserByUsername = (exports.getUserByUsername = async function (username) {
+  try {
+    const allUsers = await jsonfile.readFile(usersDB);
+    const filteredUserArray = allUsers.filter(
+      (user) => user.username === username
+    );
+    return filteredUserArray.length === 0 ? {} : filteredUserArray[0];
+  } catch (err) {
+    console.log("Error reading users: ", err.message);
+  }
+});
+
+exports.getAllUsers = async function () {
+  try {
+    const allUsers = await jsonfile.readFile(usersDB);
+    let updatedUsers = [];
+    allUsers.forEach((user) => {
+      updatedUsers.push({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+      });
     });
-  });
-}
+    return updatedUsers;
+  } catch (err) {
+    console.log("Error reading users from datastore ", err.message);
+  }
+};
+
+// Login
+
+exports.isEmptyObject = (object) => Object.entries(object).length === 0;
+
+exports.isPasswordCorrect = async function (key, password) {
+  return bcrypt.compare(password, key).then((result) => result);
+};
+
+const getUsernameFromToken = (token) => {
+  if (!token) {
+    throw new Error('Token is not provided');
+  }
+
+  const decodedToken = jwt.decode(token);
+  if (!decodedToken) {
+    throw new Error('Token is not valid');
+  }
+
+  return decodedToken["sub"];
+};
+
+exports.getAudienceFromToken = (token) => jwt.decode(token)["aud"];
+
+exports.generateToken = async function (prevToken, userName) {
+  const name = userName || getUsernameFromToken(prevToken);
+  const user = await getUserByUsername(name);
+  const options = {
+    algorithm: process.env.ALGORITHM,
+    expiresIn: process.env.EXPIRY,
+    issuer: process.env.ISSUER,
+    subject: userName || user.username,
+    audience:
+      user.role === "admin"
+        ? Constants.JWT_OPTIONS.ADMIN_AUDIENCE
+        : Constants.JWT_OPTIONS.MEMBER_AUDIENCE,
+  };
+  return jwt.sign({}, process.env.SECRET, options);
+};
+
+exports.verifyToken = (req, res, next) => {
+  if (!req.headers.authorization)
+    res.status(401).send({ message: "Not authorized to access data" });
+  else {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token)
+      res.status(401).send({ message: "Not Authorized to access data" });
+    else {
+      jwt.verify(token, process.env.SECRET, function (err) {
+        if (err) {
+          res.status(401).send({ message: "Please login again" });
+        } else next();
+      });
+    }
+  }
+};
+
+// To Remove
 
 exports.addPet = async function (pet) {
   try {
