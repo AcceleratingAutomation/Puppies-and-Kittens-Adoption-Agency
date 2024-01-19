0476de812b3a40146a237feba1eabab5273eb7ad
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-18 22:04:17 -0700

    Add vet card

---
+++ src/components/veterinarians/VeterinarianCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import "../../styles.css";
+import { SummaryCard } from "../SummaryCard";
+
+const VeterinarianCard = ({ displayName, id, email, numCurrentRescues, numTotalRescues, image, isAccepting }) => {
+  const veterinarian = { displayName, id, email, numCurrentRescues, numTotalRescues, image, isAccepting };
+
+  return (
+    <SummaryCard type={"veterinarian"} image={veterinarian.image} name={veterinarian.displayName} viewComponentDetailsUrl={`/v1/veterinarian/${veterinarian.id}`} >
+      <Grid item xs={12}>
+        <Typography variant="h4">{veterinarian.displayName}</Typography>
+      </Grid>
+      <Typography variant="h5" gutterBottom>
+        {veterinarian.email}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        New Patients: {veterinarian.isAccepting ? "Yes" : "No"}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Current Rescues: {veterinarian.numCurrentRescues}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Total Rescues: {veterinarian.numTotalRescues}
+      </Typography>
+      <Grid item xs={12} container justify="center">
+      </Grid>
+    </SummaryCard>
+  );
+};
+
+export default VeterinarianCard;
--- src/components/veterinarians/Veterinarians.js
+++ src/components/veterinarians/Veterinarians.js
@@ -2,60 +2,72 @@ import React, { useEffect, useCallback, useReducer } from "react";
 import { Grid, Typography } from "@material-ui/core";
 import "../../styles.css";
 import { AppHeader } from "../header/AppHeader";
-import { constructHeader, updateAppSettings } from "../../utils/utils";
+import { updateAppSettings } from "../../utils/utils";
 import { useHistory } from "react-router-dom";
 import { tabs } from "../header/AppHeader";
-
-const url = "http://localhost:5000/v1/veterinarians";
+import VeterinarianCard from "./VeterinarianCard";
+import { veterinariansUrl } from "../../server/api/apiConfig";
+import { fetchData } from "../../server/api/cardApi";
 
 const initialState = {
-    veterinarians: [],
-  };
+  veterinarians: [],
+};
 
 function reducer(state, action) {
-    switch (action.type) {
-      case 'setVeterinarians':
-        return { ...state, veterinarians: action.value };
-      default:
-        throw new Error();
-    }
+  switch (action.type) {
+    case 'setVeterinarians':
+      return { ...state, veterinarians: action.value };
+    default:
+      throw new Error();
   }
+}
 
 export const Veterinarians = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const history = useHistory();
-    const tabValue = tabs.findIndex(tab => tab.label === 'Veterinarians');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const history = useHistory();
+  const tabValue = tabs.findIndex(tab => tab.label === 'Veterinarians');
 
-    const redirect = useCallback(() => {
-        localStorage.clear();
-        history.push("/v1/login");
-      }, [history]);
-    
-      useEffect(() => {
-        fetch(url, { headers: constructHeader() })
-          .then((res) => (res.status === 401 ? redirect() : res.json()))
-          .then((json) => {
-            if (json) {
-              updateAppSettings(json.token);
-              dispatch({ type: 'setVeterinarians', value: [...json.veterinarians] });
-            }
-          })
-          .catch((err) => console.log("Error fetching veterinarians ", err.message));
-      }, [redirect]);
+  const redirect = useCallback(() => {
+    localStorage.clear();
+    history.push("/v1/login");
+  }, [history]);
 
-    return (
-        <div className="Content">
-          <AppHeader tabValue={tabValue} />
-          <Grid container justify="center" alignItems="center" direction="column">
-            <Grid item style={{ marginBottom: "5vh" }}>
-              <Typography variant="h3" gutterBottom>
-                Veterinarians
-              </Typography>
-            </Grid>
-            <Grid item container justify="center">
-                <p>Coming soon!</p>
-            </Grid>
-          </Grid>
-        </div>
-      );
+  useEffect(() => {
+    fetchData(veterinariansUrl)
+      .then((json) => {
+        if (json) {
+          updateAppSettings(json.token);
+          dispatch({ type: 'setVeterinarians', value: [...json.veterinarians] });
+        }
+      })
+      .catch((err) => console.log("Error fetching veterinarians ", err.message));
+  }, [redirect]);
+
+  return (
+    <div className="Content">
+      <AppHeader tabValue={tabValue} />
+      <Grid container justify="center" alignItems="center" direction="column">
+        <Grid item style={{ marginBottom: "5vh" }}>
+          <Typography variant="h3" gutterBottom>
+            Veterinarians
+          </Typography>
+        </Grid>
+        <Grid item container justify="center">
+          {state.veterinarians.map((veterinarian) => {
+            return (
+              <VeterinarianCard
+                key={veterinarian.id}
+                image={veterinarian.image}
+                displayName={veterinarian.displayName}
+                email={veterinarian.email}
+                numCurrentRescues={veterinarian.numCurrentRescues}
+                numTotalRescues={veterinarian.numTotalRescues}
+                isAccepting={veterinarian.isAccepting}
+              />
+            );
+          })}
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
