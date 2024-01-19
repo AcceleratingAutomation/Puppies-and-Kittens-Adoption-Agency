7900520c06ad451b18ad6fed7bbcc13b0fe259d7
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-18 21:49:04 -0700

    Add foster card

---
+++ src/components/fosters/FosterCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import "../../styles.css";
+import { SummaryCard } from "../SummaryCard";
+
+const FosterCard = ({ displayName, id, email, numCurrentRescues, numTotalRescues, image, isAccepting }) => {
+  const foster = { displayName, id, email, numCurrentRescues, numTotalRescues, image, isAccepting };
+
+  return (
+    <SummaryCard type={"foster"} image={foster.image} name={foster.displayName} viewComponentDetailsUrl={`/v1/foster/${foster.id}`} >
+      <Grid item xs={12}>
+        <Typography variant="h4">{foster.displayName}</Typography>
+      </Grid>
+      <Typography variant="h5" gutterBottom>
+        {foster.email}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        New Fosters: {foster.isAccepting ? "Yes" : "No"}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Current Rescues: {foster.numCurrentRescues}
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Total Rescues: {foster.numTotalRescues}
+      </Typography>
+      <Grid item xs={12} container justify="center">
+      </Grid>
+    </SummaryCard>
+  );
+};
+
+export default FosterCard;
--- src/components/fosters/Fosters.js
+++ src/components/fosters/Fosters.js
@@ -2,60 +2,72 @@ import React, { useEffect, useCallback, useReducer } from "react";
 import { Grid, Typography } from "@material-ui/core";
 import "../../styles.css";
 import { AppHeader } from "../header/AppHeader";
-import { constructHeader, updateAppSettings } from "../../utils/utils";
+import { updateAppSettings } from "../../utils/utils";
 import { useHistory } from "react-router-dom";
 import { tabs } from "../header/AppHeader";
-
-const url = "http://localhost:5000/v1/fosters";
+import { fostersUrl } from "../../server/api/apiConfig";
+import { fetchData } from "../../server/api/cardApi";
+import FosterCard from "./FosterCard";
 
 const initialState = {
-    fosters: [],
-  };
+  fosters: [],
+};
 
 function reducer(state, action) {
-    switch (action.type) {
-      case 'setFosters':
-        return { ...state, fosters: action.value };
-      default:
-        throw new Error();
-    }
+  switch (action.type) {
+    case 'setFosters':
+      return { ...state, fosters: action.value };
+    default:
+      throw new Error();
   }
+}
 
 export const Fosters = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const history = useHistory();
-    const tabValue = tabs.findIndex(tab => tab.label === 'Fosters');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const history = useHistory();
+  const tabValue = tabs.findIndex(tab => tab.label === 'Fosters');
 
-    const redirect = useCallback(() => {
-        localStorage.clear();
-        history.push("/v1/login");
-      }, [history]);
-    
-      useEffect(() => {
-        fetch(url, { headers: constructHeader() })
-          .then((res) => (res.status === 401 ? redirect() : res.json()))
-          .then((json) => {
-            if (json) {
-              updateAppSettings(json.token);
-              dispatch({ type: 'setFosters', value: [...json.fosters] });
-            }
-          })
-          .catch((err) => console.log("Error fetching fosters ", err.message));
-      }, [redirect]);
+  const redirect = useCallback(() => {
+    localStorage.clear();
+    history.push("/v1/login");
+  }, [history]);
 
-    return (
-        <div className="Content">
-          <AppHeader tabValue={tabValue} />
-          <Grid container justify="center" alignItems="center" direction="column">
-            <Grid item style={{ marginBottom: "5vh" }}>
-              <Typography variant="h3" gutterBottom>
-                Fosters
-              </Typography>
-            </Grid>
-            <Grid item container justify="center">
-                <p>Coming soon!</p>
-            </Grid>
-          </Grid>
-        </div>
-      );
+  useEffect(() => {
+    fetchData(fostersUrl)
+      .then((json) => {
+        if (json) {
+          updateAppSettings(json.token);
+          dispatch({ type: 'setFosters', value: [...json.fosters] });
+        }
+      })
+      .catch((err) => console.log("Error fetching fosters ", err.message));
+  }, [redirect]);
+
+  return (
+    <div className="Content">
+      <AppHeader tabValue={tabValue} />
+      <Grid container justify="center" alignItems="center" direction="column">
+        <Grid item style={{ marginBottom: "5vh" }}>
+          <Typography variant="h3" gutterBottom>
+            Fosters
+          </Typography>
+        </Grid>
+        <Grid item container justify="center">
+          {state.fosters.map((foster) => {
+            return (
+              <FosterCard
+                key={foster.id}
+                image={foster.image}
+                displayName={foster.displayName}
+                email={foster.email}
+                numCurrentRescues={foster.numCurrentRescues}
+                numTotalRescues={foster.numTotalRescues}
+                isAccepting={foster.isAccepting}
+              />
+            );
+          })}
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
