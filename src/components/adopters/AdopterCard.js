b1c01298b848ee1f0f855327f7868d5594b6dbeb
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-01-28 18:03:27 -0700

    Add prettier to format js, json, ts, css, md, and yml locally and with github actions

--- src/components/adopters/AdopterCard.js
+++ src/components/adopters/AdopterCard.js
@@ -3,11 +3,30 @@ import { Grid, Typography } from "@material-ui/core";
 import "../../styles.css";
 import { SummaryCard } from "../SummaryCard";
 
-const AdopterCard = ({ displayName, id, email, numHouseholdPets, image, isAdopting }) => {
-  const adopter = { displayName, id, email, numHouseholdPets, image, isAdopting };
+const AdopterCard = ({
+  displayName,
+  id,
+  email,
+  numHouseholdPets,
+  image,
+  isAdopting,
+}) => {
+  const adopter = {
+    displayName,
+    id,
+    email,
+    numHouseholdPets,
+    image,
+    isAdopting,
+  };
 
   return (
-    <SummaryCard type={"adopter"} image={adopter.image} name={adopter.displayName} viewComponentDetailsUrl={`/v1/adopter/${adopter.id}`} >
+    <SummaryCard
+      type={"adopter"}
+      image={adopter.image}
+      name={adopter.displayName}
+      viewComponentDetailsUrl={`/v1/adopter/${adopter.id}`}
+    >
       <Grid item xs={12}>
         <Typography variant="h4">{adopter.displayName}</Typography>
       </Grid>
@@ -20,8 +39,7 @@ const AdopterCard = ({ displayName, id, email, numHouseholdPets, image, isAdopti
       <Typography variant="h5" gutterBottom>
         Current Pets: {adopter.numHouseholdPets}
       </Typography>
-      <Grid item xs={12} container justify="center">
-      </Grid>
+      <Grid item xs={12} container justify="center"></Grid>
     </SummaryCard>
   );
 };
