3464898275baa9311bbf814bc7210c1e737e5501
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-02-01 22:17:29 -0700

    Fix 6 eslint warnings

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,4 @@
+const debug = require("debug")("app:pwa");
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -35,7 +36,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              debug(
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://bit.ly/CRA-PWA.",
               );
@@ -48,7 +49,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log("Content is cached for offline use.");
+              debug("Content is cached for offline use.");
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -60,7 +61,7 @@ function registerValidSW(swUrl, config) {
       };
     })
     .catch((error) => {
-      console.error("Error during service worker registration:", error);
+      throw new Error(`Error during service worker registration: ${error}`);
     });
 }
 
@@ -88,9 +89,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log(
-        "No internet connection found. App is running in offline mode.",
-      );
+      debug("No internet connection found. App is running in offline mode.");
     });
 }
 
@@ -115,7 +114,7 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
+          debug(
             "This web app is being served cache-first by a service " +
               "worker. To learn more, visit https://bit.ly/CRA-PWA",
           );
@@ -135,7 +134,7 @@ export function unregister() {
         registration.unregister();
       })
       .catch((error) => {
-        console.error(error.message);
+        throw error;
       });
   }
 }
