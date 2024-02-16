81db5cc49bc5ac6539db43704fbe411611173c17
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-02-16 16:00:28 -0700

    Upload test report even when all playwright tests haven't completed

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -13,6 +13,11 @@ export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
+  expect: {
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.01,
+    },
+  },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
