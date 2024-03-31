6f0cb58f6b7eb9aea23fdee51f9a43bd65d76229
Jason Caldwell <JasonCaldwellMBA@gmail.com>
2024-03-30 18:26:07 -0600

    Make delete details button always show on separate line

--- src/components/DetailsButtons.jsx
+++ src/components/DetailsButtons.jsx
@@ -22,37 +22,41 @@ function DetailsButtons({
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} container justify="center">
-      <Button
-        className={classes.muiButton}
-        variant="contained"
-        color="primary"
-        onClick={onBack}
-      >
-        Back
-      </Button>
-      <Button
-        className={classes.muiButton}
-        variant="contained"
-        color="primary"
-        onClick={() => onEdit(id)}
-      >
-        Edit {name}
-      </Button>
-      <Button
-        className={classes.muiButton}
-        variant="contained"
-        color="secondary"
-        onClick={() => dispatch({ type: "openDialog" })}
-      >
-        DELETE {name}
-      </Button>
-      <ConfirmationDialog
-        open={openDialog}
-        onClose={() => dispatch({ type: "closeDialog" })}
-        onConfirm={() => onDelete(id)}
-      />
-    </Grid>
+    <>
+      <Grid item xs={12} container justify="center">
+        <Button
+          className={classes.muiButton}
+          variant="contained"
+          color="primary"
+          onClick={onBack}
+        >
+          Back
+        </Button>
+        <Button
+          className={classes.muiButton}
+          variant="contained"
+          color="primary"
+          onClick={() => onEdit(id)}
+        >
+          Edit {name}
+        </Button>
+      </Grid>
+      <Grid item xs={12} container justify="center">
+        <Button
+          className={classes.muiButton}
+          variant="contained"
+          color="secondary"
+          onClick={() => dispatch({ type: "openDialog" })}
+        >
+          DELETE {name}
+        </Button>
+        <ConfirmationDialog
+          open={openDialog}
+          onClose={() => dispatch({ type: "closeDialog" })}
+          onConfirm={() => onDelete(id)}
+        />
+      </Grid>
+    </>
   );
 }
 
